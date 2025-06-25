13dc5adbac133c79d6dbb0fcc7badff0aa13c6ff
maury ribeiro <Maurygssss@hotmail.com>
2025-06-24 23:03:51 -0300

    new iasd.sonoplastia

--- IASD.Sonoplastia/Program.cs
+++ IASD.Sonoplastia/Program.cs
@@ -1,6 +1,6 @@
+using IASD.Sonoplastia;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using IASD.Sonoplastia;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -8,7 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
